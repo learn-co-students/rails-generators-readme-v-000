56d29c6ccf9dd35e54199bc77ba5966aea57d934
Lukeghenco <lukeghenco@gmail.com>
2016-01-18 14:19:39 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113163910) do
+ActiveRecord::Schema.define(version: 20160118190159) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
