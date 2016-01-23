1e81a68e89d2a8d403bb310a26f88048d2c5b8d4
Justin Thomas <jthomas0209@gmail.com>
2016-01-23 13:59:27 -0500

    Added some specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,7 @@ RSpec.configure do |config|
   end
 
   config.infer_spec_type_from_file_location!
+  config.include Capybara::DSL
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
