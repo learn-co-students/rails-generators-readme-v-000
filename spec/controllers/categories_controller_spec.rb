8e7a7ee5808092d06e488a44783b4f766d99fde2
Adam King <adam.king0126@gmail.com>
2016-02-23 10:58:06 -0600

    Done.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,10 +1,15 @@
+require 'pry'
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
   describe "GET #show" do
+    before do
+      @category = Category.create(name: "hello category")
+    end
+
     it "returns http success" do
-      get :show
+      get :show, {:id => @category.id}
       expect(response).to have_http_status(:success)
     end
   end
