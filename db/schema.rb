4131b5afa2533efd000103ba5d567b1961bfc494
pajamaw <pjpeterww@gmail.com>
2016-03-03 17:02:07 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113163910) do
+ActiveRecord::Schema.define(version: 20160303212855) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160113163910) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.boolean  "post_status"
+    t.string   "post_status"
     t.integer  "author_id"
     t.integer  "category_id"
   end
