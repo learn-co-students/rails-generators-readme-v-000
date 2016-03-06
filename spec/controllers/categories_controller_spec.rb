eb2319952966f222ff6889321cc44344356708b1
Chris Bruen <bruen.chris@gmail.com>
2016-03-06 17:03:33 -0500

    done

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :controller do
 
   describe "GET #show" do
+
     it "returns http success" do
-      get :show
+      @category = Category.create(name: "Chris", weight: 150)
+     
+      visit category_path(@category) 
       expect(response).to have_http_status(:success)
     end
   end
