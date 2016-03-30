4030e5460f1675a01a7e8f1e77b30e22e3efdd59
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-30 12:02:38 -0400

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,19 +3,13 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   it "has a name" do
     @author = Author.new
-    @author.name = "Joe Burgess"
-    expect(@author.name).to eq("Joe Burgess")
-  end
-
-  it "has a genre" do
-    @author = Author.new
-    @author.genre = "Fiction"
-    expect(@author.genre).to eq("Fiction")
+    @author.name = "Aldous Huxley"
+    expect(@author.name).to eq("Aldous Huxley")
   end
 
   it "has a bio" do
     @author = Author.new
-    @author.bio = "I'm a Teacher!"
-    expect(@author.bio).to eq("I'm a Teacher!")
+    @author.bio = "Awesome guy, really!"
+    expect(@author.bio).to eq("Awesome guy, really!")
   end
 end
