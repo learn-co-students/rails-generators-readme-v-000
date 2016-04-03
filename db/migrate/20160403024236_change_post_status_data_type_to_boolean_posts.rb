f3168c154dda571dc854f5cca20919d7cd89e37e
swdavies7 <swdavies7@gmail.com>
2016-04-03 02:49:30 +0000

    Done.

---
+++ db/migrate/20160403024236_change_post_status_data_type_to_boolean_posts.rb
@@ -0,0 +1,5 @@
+class ChangePostStatusDataTypeToBooleanPosts < ActiveRecord::Migration
+  def change
+    change_column :posts, :post_status, :boolean
+  end
+end
---
+++ spec/factories/authors.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :author do
+    name "MyString"
+    genre "MyString"
+    bio "MyText"
+  end
+end
