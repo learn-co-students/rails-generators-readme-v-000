aea88059fe589b13dd18b1be731401f676573d0d
John Carlo  Malto <jakemalto2014@gmail.com>
2016-04-20 09:36:35 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
