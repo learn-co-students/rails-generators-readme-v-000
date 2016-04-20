1acb6b347560cec635871a6ca25569d8a87c0d99
e-baker <eric@ericbaker.me>
2016-04-20 00:57:06 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -40,6 +40,6 @@ end
 describe "post" do
   it "has a post_status field" do
     @post = Post.create(title: "My Post", description: "My post desc", post_status: true)
-    expect(@post.post_status).to eq(true)
+    expect(@post.post_status).to eq("t")
   end
 end
