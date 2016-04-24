c05b736982ffaa23c7e2e08c2b65d4fb6b0dc333
azemoh <joshazemoh@gmail.com>
2016-04-25 00:01:34 +0100

    generate admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
