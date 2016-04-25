59957b1e1ec7ae5f0aa478a8a67e4ccaa7eac90c
jenjiyi <jenny.yamada@gmail.com>
2016-04-25 09:55:50 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  resources :accounts
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
