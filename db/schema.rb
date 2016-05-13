42fc87bb6bb38155ccf2ab718c62ea05959936df
gnfisher <me@gnfisher.com>
2016-05-13 13:50:10 -0300

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 20160113163910) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.text   "bio"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -23,5 +30,7 @@ ActiveRecord::Schema.define(version: 20151230232412) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
 end
