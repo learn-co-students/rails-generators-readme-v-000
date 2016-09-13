783c1e0bd40470dc3d129fed6ca676014a0f8b47
Sandy Hogan <SVern99@aol.com>
2016-09-13 19:00:58 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913220057) do
+ActiveRecord::Schema.define(version: 20160913224336) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20160913220057) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "post_status"
+    t.boolean  "post_status"
   end
 
 end
