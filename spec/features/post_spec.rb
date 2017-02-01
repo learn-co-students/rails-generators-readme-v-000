7c4cd71093bb3af050525203213762ab63ba979f
dmm89 <dmendez89@gmail.com>
2017-02-01 12:57:10 -0500

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -35,11 +35,5 @@ describe 'form' do
 
     expect(page).to have_content("My Edit")
   end
-end
 
-describe "post" do
-  it "has a post_status field" do
-    @post = Post.create(title: "My Post", description: "My post desc", post_status: true)
-    expect(@post.post_status).to eq(true)
-  end
 end
