1d3cf3107bdb968379ae0d05e817412b2002c3aa
Shmully Wolfson <shmullywolfson@gmail.com>
2017-03-06 13:49:13 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :accounts
-  resources :admin, only: [:dashboard, :stats, :financials, :settings ]
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
 end
