9911388c01c8857857ec65ab5b04a427aa5aae21
jessica fletcher <cara.kane@gmail.com>
2017-07-07 18:55:07 +0000

    Done.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do
+RSpec.describe Account, type: :model do
   # test commented out, created by me
   # pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/admin/dashboard.html.erb_spec.rb
+++ spec/views/admin/dashboard.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do
+RSpec.describe "admin/dashboard.html.erb", type: :view do
   # test commented out, created by me
   # pending "add some examples to (or delete) #{__FILE__}"
 end
