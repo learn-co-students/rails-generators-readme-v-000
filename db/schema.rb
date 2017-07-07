8e47081788589b10ed5578c40d68ff33206a3958
cs-j <cj@mail.co.de>
2017-07-06 21:28:12 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706193600) do
-
-  create_table "accounts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "payment_status"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "genre"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170706074438) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
