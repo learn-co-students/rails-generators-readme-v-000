25cf7188d9c9d1fcbd776e89a46ea9cb89479d26
Luisa Scavo <luisascavo@gmail.com>
2017-07-08 02:04:27 +0000

    Done.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  def dashboard
+  end
+
+  def stats
+  end
+
+  def financials
+  end
+end
---
+++ spec/controllers/admin_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe AdminController, type: :controller do
+
+  describe "GET #dashboard" do
+    it "returns http success" do
+      get :dashboard
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #stats" do
+    it "returns http success" do
+      get :stats
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #financials" do
+    it "returns http success" do
+      get :financials
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
