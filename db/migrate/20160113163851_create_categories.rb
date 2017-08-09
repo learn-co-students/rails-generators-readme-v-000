5eb655a9f4dfd4d28144ffb84ea61c069afcdb50
Timothy Cevallos <timcev@gmail.com>
2017-08-09 04:19:22 +0000

    Done.

--- db/migrate/20160113163851_create_categories.rb
+++ db/migrate/20160113163851_create_categories.rb
@@ -2,6 +2,8 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string :name
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
 
       t.timestamps null: false
     end
