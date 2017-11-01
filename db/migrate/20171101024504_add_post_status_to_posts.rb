f0946333d6cc8e4120092f61c9f5802c173bcbbf
Sophia H <shhubeny@gmail.com>
2017-11-01 03:03:20 +0000

    Done.

---
+++ db/migrate/20171101024504_add_post_status_to_posts.rb
@@ -0,0 +1,7 @@
+class AddPostStatusToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :post_status, :boolean
+  end
+end
+
+#rails g migration add_post_status_to_posts post_status:boolean --no-test-framework
