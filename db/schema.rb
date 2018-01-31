9bed2340332fc8a91911ec68cbf7820f94f2e656
Jess Mitchell <jessicamitch42@gmail.com>
2018-01-31 15:37:04 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130222806) do
+ActiveRecord::Schema.define(version: 20180131153341) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180130222806) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
