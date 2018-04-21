734f63f114b515cb1b2dec48da0767d318c7902a
github username <github email address>
2018-04-21 10:14:06 -0500

    Done.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  def admin
+  end
+
+  def dashboard
+  end
+
+  def status
+  end
+
+  def financial
+  end
+
+  def settings
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'main/admin'
+
+  get 'main/dashboard'
+
+  get 'main/status'
+
+  get 'main/financial'
+
+  get 'main/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
