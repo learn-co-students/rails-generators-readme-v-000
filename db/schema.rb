21a67937317672eb153cc9d5b74e61137c8cbe73
Sean Oughton <seanoughton@gmail.com>
2018-04-24 21:43:27 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424212622) do
+ActiveRecord::Schema.define(version: 20180424214242) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20180424212622) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
