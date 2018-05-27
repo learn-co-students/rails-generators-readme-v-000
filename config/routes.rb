60197e25904e89d40d0af490215ee21c380d566d
Raiet Intraub <raietsaeed@gmail.com>
2018-05-27 23:28:08 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :accounts
+  # get 'accounts/new'
+  # get 'accounts/create'
+  # get 'accounts/edit'
+  # get 'accounts/update'
+  # get 'accounts/destroy'
+  # get 'accounts/index'
+  # get 'accounts/show'
+
   get 'admin/dashboard'
 
   get 'admin/stats'
