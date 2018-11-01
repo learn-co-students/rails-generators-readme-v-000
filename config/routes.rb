0402c32a16511ac53351498298c50c2f3ad12870
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-01 19:52:47 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'admin/dashboard'
 
   get 'admin/stats'
