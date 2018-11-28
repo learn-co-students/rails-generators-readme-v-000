78794aaad11c98e2b56fe31478b255d80d1a3eb6
SMartinSci <sm674@cornell.edu>
2018-11-28 05:02:55 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181128125659) do
+ActiveRecord::Schema.define(version: 20181128130104) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20181128125659) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
