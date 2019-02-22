cc1eae136e43a490a194a37b9562f76c7707cacf
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-22 18:37:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
+  resources :accounts
+
 end
+
+
+#rails g migration authors name:string name:genre name:bio --no-test-framework
---
+++ db/migrate/20190221025100_authors.rb
@@ -0,0 +1,11 @@
+class Authors < ActiveRecord::Migration
+
+    def change
+      create_table :authors do |t|
+        t.string :name
+        t.string :genre
+        t.string :bio
+
+    end
+  end
+end
