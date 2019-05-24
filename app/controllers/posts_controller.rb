3f46b49d1202d1b77cb9d69c8b907aaef5e2029e
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-24 14:02:42 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,35 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def show
-  	@post = Post.find(params[:id])
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  def new
-  	@post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def create
-    @post = Post.new(params)
+	def create
+	  @post = Post.new(post_params(:title, :description))
     @post.save
     redirect_to post_path(@post)
-  end
+	end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+	def update
+  	@post = Post.find(params[:id])
+    @post.update(post_params(:title))
     redirect_to post_path(@post)
-  end
+	end
 
-  def edit
-    @post = Post.find(params[:id])
+	def edit
+  	@post = Post.find(params[:id])
+	end
+	
+private
+ 
+  def post_params(*args)
+    params.require(:post).permit(*args)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'posts/:id', to: 'posts#update'
+  resources :posts, only: %i[index show new create edit update]
   resources :categories
 end
