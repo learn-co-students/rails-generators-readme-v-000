66c92014b21fcfbc37e2a039a67d684d885dd1da
sdcro <sdcrouse91@gmail.com>
2019-11-23 17:15:03 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191123092623) do
+ActiveRecord::Schema.define(version: 20191124010402) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
