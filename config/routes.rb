a7eb4468cd6bfcd32db6621a6944efe5b5cedc5a
Laura Messa Travin <laura.travin@gmail.com>
2021-03-18 23:36:37 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
